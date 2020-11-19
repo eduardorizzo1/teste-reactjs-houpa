abc6a086b11cb3621088c4484369df0845d40e7f
Eduardo <eduardorizzo1@outlook.com>
2020-11-19 11:57:03 -0300

    end of layout

--- src/pages/Purchase/index.tsx
+++ src/pages/Purchase/index.tsx
@@ -1,9 +1,34 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
-import { Container, TitleLink } from './style'
+import {
+  FaFacebookSquare,
+  FaInstagramSquare,
+  FaLinkedin,
+  FaPinterestSquare,
+  FaWhatsappSquare,
+} from 'react-icons/fa'
 
 import imgProduct from '../../assets/product-top@2x.png'
+import {
+  Container,
+  TitleLink,
+  ContainerPurchase,
+  PurchaseDescription,
+  ImgContainer,
+  ProductImgDetails,
+  ValueContainer,
+  SizeContainer,
+  SizeOptions,
+  QuantityContainer,
+  SelectQuantity,
+  ButtonsContainer,
+  ButtonBuy,
+  ButtonShoppingCart,
+  ShippingContainer,
+  ShippingInput,
+  SocialContainer,
+  SocialNetworks,
+} from './style'
 
 const Purchase: React.FC = () => {
   return (
@@ -20,7 +45,83 @@ const Purchase: React.FC = () => {
         </div>
       </TitleLink>
 
-      <img src={imgProduct} alt="img-product" />
+      <ContainerPurchase>
+        <ImgContainer>
+          <img src={imgProduct} alt="img-product" />
+          <ProductImgDetails>
+            <h4>- Camiseta masculina</h4>
+            <h4>- Manga curta</h4>
+            <h4>- Gola redonda</h4>
+            <h4>- Estampa super herói escudo capitão</h4>
+            <h4>- Tecido: meia malha</h4>
+            <h4>- Composição: 100% algodão</h4>
+            <h4>- Modelo veste tamanho: M</h4>
+          </ProductImgDetails>
+        </ImgContainer>
+
+        <PurchaseDescription>
+          <h5>Vestidos</h5>
+          <h4>Vestido curto</h4>
+          <h6>
+            Produzido e entregue por Madame Ristow <br /> Vendido por: Pantheon
+          </h6>
+
+          <ValueContainer>
+            <h4>R$ 46,13</h4>
+            <h6>ou até 5x de R$ 9,03 | Atacado mínimo: 6 peças</h6>
+          </ValueContainer>
+
+          <SizeContainer>
+            <h6>Tamanho</h6>
+            <SizeOptions>
+              <p>P</p>
+              <p>M</p>
+              <p>G</p>
+              <p>GG</p>
+            </SizeOptions>
+          </SizeContainer>
+
+          <QuantityContainer>
+            <h6>Quantidade</h6>
+
+            <SelectQuantity>
+              <select>
+                <option>1</option>
+              </select>
+              <h6>Avise-me quando chegar</h6>
+            </SelectQuantity>
+          </QuantityContainer>
+
+          <ButtonsContainer>
+            <ButtonBuy>Comprar</ButtonBuy>
+            <ButtonShoppingCart>Adicionar ao Carrinho</ButtonShoppingCart>
+          </ButtonsContainer>
+
+          <ShippingContainer>
+            <h4>Frete</h4>
+            <h6>Calcule o frete estimado para sua região</h6>
+
+            <ShippingInput>
+              <input type="text" placeholder="CEP" />
+              <button>Calcular</button>
+            </ShippingInput>
+
+            <small>Não sei meu cep</small>
+          </ShippingContainer>
+
+          <SocialContainer>
+            <h6>Compartilhar</h6>
+
+            <SocialNetworks>
+              <FaWhatsappSquare size={20} />
+              <FaPinterestSquare size={20} />
+              <FaInstagramSquare size={20} />
+              <FaFacebookSquare size={20} />
+              <FaLinkedin size={20} />
+            </SocialNetworks>
+          </SocialContainer>
+        </PurchaseDescription>
+      </ContainerPurchase>
     </Container>
   )
 }
--- src/pages/Purchase/style.ts
+++ src/pages/Purchase/style.ts
@@ -3,18 +3,13 @@ import styled from 'styled-components'
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
-
-  img {
-    margin-top: 20px;
-    width: 50%;
-    height: 100%;
-  }
 `
 
 export const TitleLink = styled.div`
   display: flex;
   flex-direction: row;
   margin-top: 60px;
+  margin-bottom: 20px;
 
   h4 {
     font-size: 12px;
@@ -24,5 +19,222 @@ export const TitleLink = styled.div`
     display: flex;
     flex-direction: row;
     color: #979797;
+  }
+`
+
+export const ContainerPurchase = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+`
+
+export const PurchaseDescription = styled.div`
+  width: 50%;
+  padding: 10px 200px;
+
+  display: flex;
+  flex-direction: column;
+
+  h5 {
+    font-family: 'Roboto';
+    color: #707070;
+    font-size: 16px;
+    margin-bottom: 11px;
+  }
+
+  h4 {
+    font-family: 'Poppins';
+    font-size: 20px;
+    margin-bottom: 6px;
+  }
+
+  h6 {
+    font-family: 'Roboto';
+    font-size: 12px;
+    color: #222222;
+  }
+`
+
+export const ImgContainer = styled.div`
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+
+  img {
+    width: 100%;
+  }
+`
+export const ProductImgDetails = styled.div`
+  padding: 18px;
+
+  h4 {
+    font-family: 'Roboto';
+    font-size: 14px;
+    color: #222222;
+    margin: 2px 0;
+  }
+`
+
+export const ValueContainer = styled.div`
+  margin-top: 17px;
+
+  h4 {
+    font-family: 'Poppins';
+    font-size: 24px;
+  }
+
+  h6 {
+    font-family: 'Roboto';
+    font-size: 12px;
+    color: #707070;
+  }
+`
+
+export const SizeContainer = styled.div`
+  margin-top: 17px;
+`
+
+export const SizeOptions = styled.div`
+  display: flex;
+  flex-direction: row;
+
+  h6 {
+    font-family: 'Roboto';
+    font-size: 12px;
+  }
+
+  p {
+    margin-top: 5px;
+    font-family: 'Roboto';
+    font-size: 14px;
+    margin-right: 18px;
+    padding: 6px;
+    color: #707070;
+    background-color: #e9e9e9;
+    border-radius: 4px;
+  }
+`
+
+export const QuantityContainer = styled.div`
+  h6 {
+    font-family: 'Roboto';
+    font-size: 16px;
+    color: #222222;
+  }
+
+  margin-top: 39px;
+`
+
+export const SelectQuantity = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+
+  h6 {
+    font-family: 'Roboto';
+    font-size: 12px;
+    padding: 16px;
+  }
+
+  select {
+    background-color: #fff;
+    border-radius: 4px;
+    height: 40px;
+    width: 80px;
+    text-align-last: center;
+    font-family: 'Poppins';
+    font-size: 14px;
+
+    option {
+      text-align-last: center;
+    }
+  }
+`
+
+export const ButtonsContainer = styled.div`
+  margin: 25px 0 50px 0;
+`
+
+export const ButtonBuy = styled.button`
+  width: 100%;
+  border-radius: 4px;
+  background-color: #000;
+  color: #fff;
+  border: 0;
+  height: 48px;
+  margin-bottom: 10px;
+  font-family: 'Roboto';
+  font-size: 18px;
+`
+
+export const ButtonShoppingCart = styled.button`
+  width: 100%;
+  border-radius: 4px;
+  background-color: #fff;
+  color: #000;
+  height: 48px;
+  border: solid 1px #707070;
+  font-family: 'Roboto';
+  font-size: 18px;
+`
+
+export const ShippingContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  h4 {
+    font-family: 'Poppins';
+    font-size: 20px;
+  }
+
+  h6 {
+    font-family: 'Roboto';
+    font-size: 14px;
+  }
+
+  small {
+    font-family: 'Roboto';
+    font-size: 12px;
+    color: #222222;
+    margin-top: 10px;
+  }
+`
+
+export const ShippingInput = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-top: 5px;
+  width: 100%;
+
+  input {
+    border-radius: 4px;
+    background-color: #fff;
+    border: solid 1px #e9e9e9;
+    height: 40px;
+    padding: 6px 16px;
+    width: 60%;
+  }
+
+  button {
+    margin-left: 16px;
+    border-radius: 4px;
+    background-color: #000;
+    border: 0;
+    color: #fff;
+    font-family: 'Roboto';
+    font-size: 16px;
+    padding: 10px 16px;
+  }
+`
+
+export const SocialContainer = styled.div`
+  margin-top: 50px;
+`
+
+export const SocialNetworks = styled.div`
+  margin-top: 8px;
+
+  svg {
+    margin-right: 8px;
   }
 `
